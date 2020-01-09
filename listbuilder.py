4435f0656831495b17ae7a8964103d134839c94d
ardaedhel <sprint_ska@hotmail.com>
2020-01-09 04:05:48 -0800

    Trying to fix chat again

--- listbuilder.py
+++ listbuilder.py
@@ -282,8 +282,8 @@ def import_from_list(import_from,output_to,working_path,conn,isvlog=False):
 
         with open(output_to,"w") as vlb:
             vlb.write("a1\r\nbegin_save{}\r\nend_save{}\r\n".format(chr(27),chr(27)))
-            vlb.write("LOG CHAT<Listbuilder> - Fleet imported by Shrimpbot on the Armada Discord.{}\r\n".format(chr(27))+\
-                      "LOG CHAT<Listbuilder> - https://discord.gg/jY4K4d6{}\r\n\r\n".format(chr(27)))
+            vlb.write("LOG\tCHAT<Listbuilder> - Fleet imported by Shrimpbot on the Armada Discord.{}\r\n".format(chr(27))+\
+                      "LOG\tCHAT<Listbuilder> - https://discord.gg/jY4K4d6{}\r\n\r\n".format(chr(27)))
             for s in f.ships:
                 vlb.write(s.shipcard.content+chr(27))
                 vlb.write(s.shiptoken.content+chr(27))
