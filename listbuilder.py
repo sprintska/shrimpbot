8a1e6d4dba7d891fe1c1a7d261ba3e309545acec
Ardaedhel <ardaedhel@MYRMIDON.LAN>
2020-11-16 00:32:49 -0800

    ... linting is good

--- listbuilder.py
+++ listbuilder.py
@@ -470,11 +470,14 @@ def import_from_warlords(import_list, vlb_path, working_path, conn):
 
     # Make sure the cretinous user isn't just schwacking off all the garbage at
     # the top of a Warlords export.
-    ship_check = import_list.split("\n")[0].strip().split[0]
+    ship_check = import_list.split("\n")[0].strip().split()[0]
     if len(ship_check) >= 3:
-        ship_query = conn.execute("SELECT piecetype FROM pieces where piecename LIKE ?""",('%'+ship_check+'%',)).fetchall
+        ship_query = conn.execute(
+            "SELECT piecetype FROM pieces where piecename LIKE ?" "",
+            ("%" + ship_check + "%",),
+        ).fetchall
     if len(ship_query) > 0:
-        if ('ship',) in ship_query or ('shipcard',) in ship_query:
+        if ("ship",) in ship_query or ("shipcard",) in ship_query:
             shipnext = True
 
     for line in import_list.split("\n"):
