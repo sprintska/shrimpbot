5786112944baeca08d442d08820f278a6e4f3896
ardaedhel <sprint_ska@hotmail.com>
2021-04-24 04:36:49 -0700

    #73

--- listbuilder.py
+++ listbuilder.py
@@ -239,6 +239,8 @@ ambiguous_names = {
     ("landocalrissian", "4"): ("landocalrissian", "upgrade"),
     ("leiaorgana", "28"): ("leiaorganacom", "upgrade"),
     ("leiaorgana", "3"): ("leiaorganaoff", "upgrade"),
+    ("luminaraundili", "23"): ("luminaraundili", "squadron"),
+    ("luminaraundili", "25"): ("luminaraundilicom", "upgrade")
     ("plokoon", "26"): ("plokooncom", "upgrade"),
     ("plokoon", "24"): ("plokoon", "squadron"),
     ("wedgeantilles", "19"): ("wedgeantilles", "squadron"),
