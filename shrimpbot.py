22d3675e797c2931e7f1c1eeab3b85f5c7053932
Ardaedhel <sprint_ska@hotmail.com>
2025-07-22 22:46:30 -0700

    somehow a bug returned

--- shrimpbot.py
+++ shrimpbot.py
@@ -497,8 +497,6 @@ async def on_message(message):
                 vlogfilepath = os.path.join(outpath, guid + ".vlog")
                 databasepath = os.path.join(listbuilderpath, "vlb_pieces.vlo")
 
-                conn = databasepath
-
                 listbuilder_config = listbuilder.get_default_config()
                 listbuilder_config.pwd = os.path.dirname(__file__)
                 listbuilder_config.vlog_path = vlogfilepath
@@ -529,7 +527,7 @@ async def on_message(message):
                     await message.channel.send(last_item)
 
                 else:
-                    listbuilder.export_to_vlog(vlogfilepath, vlbfilepath, workingpath)
+                    listbuilder.export_to_vlog(listbuilder_config)
                     await message.channel.send(file=discord.File(vlogfilepath))
                     await message.author.send(
                         "For usage instructions, pm me '!listhelp'."
